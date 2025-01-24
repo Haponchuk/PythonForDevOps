210f7a06c462814eb2e7c2220a23ad57eb29ac16
Haponchuk <1seldow1@gmail.com>
2025-01-24 21:34:09 +0100

    add string exercises

---
+++ chapter 02/name_cases_2.py
@@ -0,0 +1,7 @@
+# 2-4. Name Cases: Use a variable to represent a person’s name, and then print
+# that person’s name in lowercase, uppercase, and title case.
+
+first_name = 'ErEn'
+print(first_name.lower())
+print(first_name.upper())
+print(first_name.title())
---
+++ chapter_02/simple_message.py
@@ -0,0 +1,8 @@
+#print('Hello World!')
+#message = 'Hello World!'
+#print(message)
+
+message = '123qwe'
+print(message)
+message = '010101010sss'
+print(message)
