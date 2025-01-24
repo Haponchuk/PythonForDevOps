2c7f8c7e807df4cab39cb4bb909f571784d7b523
Haponchuk <1seldow1@gmail.com>
2025-01-25 00:15:53 +0100

    Start chapter_03 and solve tasks from 3-1 to 3-6

---
+++ chapter_03/lists.py
@@ -0,0 +1,24 @@
+# 3-1
+friends = ['Frank', 'Melodie', 'Gus', 'John']
+print(friends[0], friends[1], friends[2], friends[3])
+
+# 3-2
+friends = ['Frank', 'Melodie', 'Gus', 'John']
+print(f'Hi there. \n{friends[0]} loves hammers.\n{friends[1]} loves candies.\n{friends[2]} loves balloons.\n{friends[3]} loves work in factory.')
+
+# 3-3
+gpu = ['rtx5090', 'rtx5080', 'rtx5070']
+print(f'I would like to own a {gpu[0]} or {gpu[1]} or at least {gpu[2]}')
+
+# 3-1. Names: Store the names of a few of your friends in a list called names. Print
+# each person’s name by accessing each element in the list, one at a time.
+
+# 3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just
+# printing each person’s name, print a message to them. The text of each mes-
+# sage should be the same, but each message should be personalized with the
+# person’s name.
+
+# 3-3. Your Own List: Think of your favorite mode of transportation, such as a
+# motorcycle or a car, and make a list that stores several examples. Use your list
+# to print a series of statements about these items, such as “I would like to own a
+# Honda motorcycle.”
