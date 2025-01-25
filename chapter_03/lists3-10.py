cb437431c6a1db0826b8882b922e69bdbc727b95
Haponchuk <1seldow1@gmail.com>
2025-01-25 15:03:36 +0100

    Add list3-10.py and complete tasks 3-8 - 3-10

---
+++ chapter_03/lists3-10.py
@@ -0,0 +1,56 @@
+# 3-8
+countriesToVisit = ['Japan', 'Netherlands', 'Ireland', 'Switzerland', 'Australia']
+print(countriesToVisit)
+print(sorted(countriesToVisit))
+print(countriesToVisit)
+print(sorted(countriesToVisit, reverse=True))
+print(countriesToVisit)
+countriesToVisit.reverse()
+print(countriesToVisit)
+countriesToVisit.reverse()
+print(countriesToVisit)
+countriesToVisit.sort()
+print(countriesToVisit)
+countriesToVisit.sort(reverse=True)
+print(countriesToVisit)
+print('___________________________________________________')
+
+# 3-9
+guests4 = ['Bibi', 'Jess', 'Leon', 'Spike', 'Mortis', 'Crow']
+print(f'I\'m inviting {len(guests4)} people to have a dinner with me')
+print('___________________________________________________')
+
+# 3-10
+randomStuff = ['bike', 'ride', 'hike', 'permit', 'work', 'train', 'souce', 'alone', 'akward']
+sorted(randomStuff)
+sorted(randomStuff, reverse=True)
+len(randomStuff)
+
+# 3-8. Seeing the World: Think of at least five places in the world you’d like to
+# visit.
+# • Store the locations in a list. Make sure the list is not in alphabetical order.
+# • Print your list in its original order. Don’t worry about printing the list neatly,
+# just print it as a raw Python list.
+# • Use sorted() to print your list in alphabetical order without modifying the
+# actual list.
+# • Show that your list is still in its original order by printing it.
+# • Use sorted() to print your list in reverse alphabetical order without chang-
+# ing the order of the original list.
+# • Show that your list is still in its original order by printing it again.
+# • Use reverse() to change the order of your list. Print the list to show that its
+# order has changed.
+# • Use reverse() to change the order of your list again. Print the list to show
+# it’s back to its original order.
+# • Use sort() to change your list so it’s stored in alphabetical order. Print the
+# list to show that its order has been changed.
+# • Use sort() to change your list so it’s stored in reverse alphabetical order.
+# Print the list to show that its order has changed.
+
+# 3-9. Dinner Guests: Working with one of the programs from Exercises 3-4
+# through 3-7 (page 42), use len() to print a message indicating the number
+# of people you are inviting to dinner.
+
+# 3-10. Every Function: Think of something you could store in a list. For example,
+# you could make a list of mountains, rivers, countries, cities, languages, or any-
+# thing else you’d like. Write a program that creates a list containing these items
+# and then uses each function introduced in this chapter at least once.
